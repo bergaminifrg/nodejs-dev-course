09677c186ae0525292e3e39df820080425da12db
Felipe Rodrigues Goulart Bergamini <felipebergamini@aidax.com.br>
2018-09-14 18:26:12 -0300

    Section 13, Lecture 13 starting

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
+// Node Modules //
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
+// User created modules //
 const notes = require('./notes.js');
-
-// const user = os.userInfo();
-// '' or "" for normal text, `` for text with variables inside
-// fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}!`, function (err){
-//     if (err) {
-//         console.log('Unable to write to file'); //Unable to call an asynchronous function without callback
-//     }
-// });
-
-console.log(notes.add(20,-5));
-
