d5ffab0be63bd550bfe64a67962331cc413c584f
Felipe Rodrigues Goulart Bergamini <felipebergamini@aidax.com.br>
2018-09-17 10:18:45 -0300

    Starting - Section 3, Lecture 16

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 // Node Modules //
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 // User created modules //
 const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs',argv);
+
+switch(command) {
+    case 'add':
+        notes.addNote(argv.title, argv.body);
+        break;
+    case 'list':
+        notes.getAll();
+        break;
+    case 'remove':
+        notes.removeNote(argv.title);
+        break;
+    case 'read':
+        notes.getNote(argv.title);
+        break;
+    default:
+        console.log('Command not recognized');
+        break;
+}
--- notes.js
+++ notes.js
@@ -1,5 +1,24 @@
 // var = function() {}
 // var = () => {}
-module.exports.add = (number_x, number_y) => {
+var add = (number_x, number_y) => {
     return (number_x + number_y);
+}
+var addNote = (title, body) => {
+    console.log('Title:',title, ' Body:', body);
+}
+var getAll = () => {
+    console.log('Getting all notes');
+}
+var getNote = (title) => {
+    console.log('Getting',title);
+}
+var removeNote = (title) => {
+    console.log('Removing',title);
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 }
